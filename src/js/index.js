66925c89ab98ca466a5f5a0ff4cc45029d34ac91
Branson <bransonlee0426@gmail.com>
2019-03-22 13:31:41 +0800

    all bugs fixed

--- src/js/index.js
+++ src/js/index.js
@@ -28,31 +28,22 @@ $(document).ready(function () {
         .then(function (response) {
             dataArray$ = dataArray(response.data);
             setCountyDate(dataArray$);
-            
+
             setTimeout(() => {
                 $('#load-id').addClass('available');
                 $('body').css('background-color', '#f0f0f0');
                 $('#wrap-id').removeClass('available');
                 $('#footer-id').removeClass('available');
+                $('#time').text(response.data[0].PublishTime);
             }, 500);
 
-            
+
         })
 
         .catch(function (error) {
             console.log('取得資料失敗:' + error);
         });
 
-    getTime = () => {
-        let myTime = new Date();
-        let myYear = myTime.getFullYear();
-        let myMonth = myTime.getMonth();
-        let myDay = myTime.getDate();
-        let myHour = myTime.getHours();
-        let myMinutes = myTime.getMinutes();
-        $('#time').text(myYear + '/' + myMonth + '/' + myDay + ' ' + myHour + ':' + myMinutes);
-    }
-    getTime();
 
     let dataArray = (data) => {
         //get data's lenght
@@ -75,7 +66,8 @@ $(document).ready(function () {
                 CO: data[i].CO,
                 SO2: data[i].SO2,
                 NO2: data[i].NO2,
-                Status: data[i].Status
+                Status: data[i].Status,
+                PublishTime: data[i].PublishTime
             }
             array.push(myObj);
         }
@@ -105,6 +97,9 @@ $(document).ready(function () {
     }
     $('.selectbox').on('change', function () {
         $('.locations').empty();
+        //如果有第二個class就刪除
+
+
         let result = dataArray$.filter((item, index, array) => {
             return item.County === $(this).val() ? true : false;
         });
@@ -131,6 +126,7 @@ $(document).ready(function () {
                     color = 'lv6'
                     break;
                 default:
+                    color = '';
                     console.log('數值過低或超標啦！');
                     break;
             }
@@ -140,17 +136,20 @@ $(document).ready(function () {
                 '                            <span id="location-id" class="locations-aqi huge-font-size ' + color + '">' + item.AQI + '</span>' +
                 '                        </li>';
             $('.locations').append(locList);
+            //左邊顏色區塊
+            index === 0 ? $('#aside-aqi-id').removeClass() & $('#aside-aqi-id').addClass('aside-aqi' + ' ' + color) : false;
+
         });
 
-        $('.aside-aqi').addClass(color);
-        $('#o3').text(result[0].O3);
-        $('#pm10').text(result[0].PM10);
-        $('#pm25').text(result[0]['PM2.5']);
-        $('#co').text(result[0].CO);
-        $('#so2').text(result[0].SO2);
-        $('#no2').text(result[0].NO2);
-        $('.aside-aqi').text(result[0].AQI);
-        $('.aside-name').text(result[0].SiteName);
+
+        $('#o3').text(result[0].O3===''?'-':result[0].O3);
+        $('#pm10').text(result[0].PM10===''?'-':result[0].PM10);
+        $('#pm25').text(result[0]['PM2.5']===''?'-':result[0]['PM2.5']);
+        $('#co').text(result[0].CO===''?'-':result[0].CO);
+        $('#so2').text(result[0].SO2===''?'-':result[0].SO2);
+        $('#no2').text(result[0].NO2===''?'-':result[0].NO2);
+        $('.aside-aqi').text(result[0].AQI===''?'-':result[0].AQI);
+        $('.aside-name').text(result[0].SiteName===''?'-':result[0].SiteName);
         $('#title-county').text($(this).val());
 
     });
@@ -158,22 +157,26 @@ $(document).ready(function () {
     $('.locations').on('click', '.locations-list', function () {
 
         let curname = $(this).find('.locations-name').text(); //點到的地點
+        let curcolor = $(this).find('.locations-aqi').attr('class').split(' ').pop();
         let result = dataArray$.filter((item, index, array) => {
             return item.County === $('.selectbox').val() ? true : false;
         });
+        $('#aside-aqi-id').removeClass();
+        $('#aside-aqi-id').addClass('aside-aqi' + ' ' + curcolor);
 
         result.forEach((element, index, array) => {
-
             if (element.SiteName === curname) {
-                $('#o3').text(result[index].O3);
-                $('#pm10').text(result[index].PM10);
-                $('#pm25').text(result[index]['PM2.5']);
-                $('#co').text(result[index].CO);
-                $('#so2').text(result[index].SO2);
-                $('#no2').text(result[index].NO2);
+                $('#o3').text(result[index].O3===''?'-':result[index].O3);
+                $('#pm10').text(result[index].PM10===''?'-':result[index].PM10);
+                $('#pm25').text(result[index]['PM2.5']===''?'-':result[index]['PM2.5']);
+                $('#co').text(result[index].CO===''?'-':result[index].CO);
+                $('#so2').text(result[index].SO2===''?'-':result[index].SO2);
+                $('#no2').text(result[index].NO2===''?'-':result[index].NO2);
+                $('.aside-name').text(result[index].SiteName===''?'-':result[index].SiteName);
+                $('.aside-aqi').text(result[index].AQI===''?'-':result[index].AQI);
             }
         });
-        
+
     });
 
 });
